76895e992cdfffa9fe042f53736d11e8866688e4
Adhip-choudhury <79558085+Adhip-choudhury@users.noreply.github.com>
2021-10-13 20:46:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,10 @@
  leftWristy=0;
  rightWristy=0;
 
+
+ leftWrist_Socre=0;
+ rightWrist_Socre=0;
+
 function preload(){
    music=loadSound("music.mp3");
 }
@@ -24,6 +28,64 @@ function setup(){
 function draw(){
    image(video, 0, 0, 400, 300);
 
+
+   fill("#ff391e");
+   stroke("#ff391e");
+   if(leftWrist_Socre > 0.2){
+
+  
+   circle(leftWristx, leftWristy, 20);
+
+   leftWristy_number=Number(leftWristy);
+   leftWristy_without_decimals=floor(leftWristy_number);
+   volume=leftWristy_without_decimals/800;
+  
+   document.getElementById("volumename").innerHTML="Volume: "+volume;
+   music.setVolume(volume);
+   console.log("volume: "+volume);
+
+  }
+
+  if(rightWrist_Socre > 0.2){
+
+  
+   circle(rightWristx, rightWristy, 20);
+   if(rightWristy > 0 && rightWristy <= 60){
+     music.rate(0.5);
+
+     document.getElementById("speed").innerHTML="Speed: 0.5x";
+   }
+
+   else if(rightWristy > 60 && rightWristy <= 120){
+      music.rate(1);
+ 
+      document.getElementById("speed").innerHTML="Speed: 1x";
+    }
+ 
+
+    else if(rightWristy > 120 && rightWristy <= 180){
+      music.rate(1.5);
+ 
+      document.getElementById("speed").innerHTML="Speed: 1.5x";
+    }
+
+    
+    else if(rightWristy > 180 && rightWristy <= 240){
+      music.rate(2);
+ 
+      document.getElementById("speed").innerHTML="Speed: 2x";
+    }
+
+
+    else if(rightWristy > 240 && rightWristy <= 300){
+      music.rate(2.5);
+ 
+      document.getElementById("speed").innerHTML="Speed: 2.5x";
+    }
+ 
+
+
+  }
 }
 
 
@@ -42,13 +104,20 @@ function modelLoaded(){
 function gotResult(results){
     if(  results.length > 0){
       console.log(results);
-      leftWristx=results[0].pose.leftWrist.x.toFixed(0);
-      leftWristy=results[0].pose.leftWrist.y.toFixed(0);
-      console.log("leftWristx: " +leftWristx+ " leftWristy: " +leftWristy );
+      leftWristx=results[0].pose.leftWrist.x;
+      leftWristy=results[0].pose.leftWrist.y;
+      console.log("leftWristx: " +leftWristx+ " leftWristy: " +leftWristy);
 
       
-      rightWristx=results[0].pose.rightWrist.x.toFixed(0);
-      rightWristy=results[0].pose.rightWrist.y.toFixed(0);
-      console.log("rightWristx: " +rightWristx+ " rightWristy: " +rightWristy );
+      rightWristx=results[0].pose.rightWrist.x;
+      rightWristy=results[0].pose.rightWrist.y;
+      console.log("rightWristx: " +rightWristx+ " rightWristy: " +rightWristy);
+
+      leftWrist_Socre=results[0].pose.keypoints[9].score;
+      rightWrist_Socre=results[0].pose.keypoints[10].score;
+
+
+      console.log("leftWrist_score: "+leftWrist_Socre);
+      console.log("righWrist_score: "+rightWrist_Socre);
     }  
 }
